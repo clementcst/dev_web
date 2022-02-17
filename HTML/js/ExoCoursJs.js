93a44281b2ef4fbf969bb4bcb7a4631f7ecaf17f
Clément <82080806+Clemfraiche@users.noreply.github.com>
2022-02-17 22:44:30 +0100

    Add files via upload

---
+++ HTML/js/ExoCoursJs.js
@@ -0,0 +1,18 @@
+//Exercie 5
+/*
+var x = parseInt(prompt('Veuillez entrer un nombre :'));
+for(i=0;i<x;i++){
+    alert('Nombre :'+i);
+}*/
+
+//Exercice 6 
+/*
+var x = parseInt(prompt('Veuillez entrer un nombre :'));
+alert('Voici la table de multiplication de '+x +':');
+for(i=0;i<10;i++){
+    var z = i * x;
+    alert(i+' x '+x+' = '+z);
+}*/
+
+//Exercice 7
+
